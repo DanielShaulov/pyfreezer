d1f9a29ce4d8bae717de65f98a889a5bdef33437
Daniel Shaulov <daniel.shaulov@gmail.com>
2019-12-26 16:36:07 +0200

    Install dependencies before running freeze

--- freeze.sh
+++ freeze.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./Python-3.8.1/python -m pip install -r $(dirname $1)/requirements.txt
 ./Python-3.8.1/python ./Python-3.8.1/Tools/freeze/freeze.py -p ${PWD}/Python-3.8.1 -e ${PWD}/ext -o build "$@"
 cd build && make && cd -
 cp build/$(basename $1 .py) $(dirname $1)
