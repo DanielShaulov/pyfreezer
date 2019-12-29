2381a4335028764d49a86a680c3d5b0b21a2cb5f
Daniel Shaulov <daniel.shaulov@gmail.com>
2019-12-30 00:18:45 +0200

    Print earliest glibc and ldd after freezing

--- pyfreezer/freeze.sh
+++ pyfreezer/freeze.sh
@@ -4,3 +4,7 @@
 ./Python-3.8.1/python ./Python-3.8.1/Tools/freeze/freeze.py -p ${PWD}/Python-3.8.1 -e ${PWD}/ext -o build "$@"
 cd build && make && cd -
 cp build/$(basename $1 .py) $(dirname $1)
+echo "Earliest supported glibc:"
+readelf --syms build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1
+echo "ldd:"
+ldd build/$(basename $1 .py)
