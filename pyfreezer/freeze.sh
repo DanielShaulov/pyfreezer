268c965fc9b69b75279cce09302cd84eefa012f9
Daniel Shaulov <daniel.shaulov@gmail.com>
2019-12-30 00:50:47 +0200

    Print the symbols that cause highest glibc version

--- pyfreezer/freeze.sh
+++ pyfreezer/freeze.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 ./Python-3.8.1/python -m pip install -r $(dirname $1)/requirements.txt
-./Python-3.8.1/python ./Python-3.8.1/Tools/freeze/freeze.py -p ${PWD}/Python-3.8.1 -e ${PWD}/ext -o build "$@"
+./Python-3.8.1/python ./Python-3.8.1/Tools/freeze/freeze.py -p $(realpath Python-3.8.1) -e $(realpath ext) -o build "$@"
 cd build && make && cd -
 cp build/$(basename $1 .py) $(dirname $1)
-echo "Earliest supported glibc:"
-readelf --syms build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1
+GLIBC_VER=$(readelf --syms build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
+echo "Earliest supported glibc: $GLIBC_VER"
+readelf --syms build/$(basename $1 .py) | grep GLIBC_${GLIBC_VER}
 echo "ldd:"
 ldd build/$(basename $1 .py)
