1568b3dc0308fbc84d027825ae52c54bc4c05f71
Daniel Shaulov <daniel.shaulov@gmail.com>
2020-01-04 01:19:29 +0200

    Remove the last few functions above glibc 2.10

--- pyfreezer/freeze.sh
+++ pyfreezer/freeze.sh
@@ -2,9 +2,11 @@
 
 ./Python-3.8.1/python -m pip install -r $(dirname $1)/requirements.txt
 ./Python-3.8.1/python ./Python-3.8.1/Tools/freeze/freeze.py -p $(realpath Python-3.8.1) -e $(realpath ext) -o build "$@"
+cp overrides.c build/
+export LDFLAGS=overrides.c
 cd build && make && cd -
 cp build/$(basename $1 .py) $(dirname $1)
-GLIBC_VER=$(readelf --dyn-syms build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
+GLIBC_VER=$(readelf --dyn-syms --wide build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
 echo "Earliest supported glibc: $GLIBC_VER"
 readelf --syms build/$(basename $1 .py) | grep GLIBC_${GLIBC_VER}
 echo "ldd:"
---
+++ pyfreezer/overrides.c
@@ -0,0 +1,13 @@
+#include <sys/select.h>
+extern void __chk_fail (void) __attribute__ ((__noreturn__));
+
+long int
+__fdelt_chk (long int d)
+{
+  if (d < 0 || d >= FD_SETSIZE)
+    __chk_fail ();
+
+  return d / __NFDBITS;
+}
+
+__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
