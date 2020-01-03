6fc06966c935805270e4ea03333c6308d9b44af5
Daniel Shaulov <daniel.shaulov@gmail.com>
2020-01-04 01:57:17 +0200

    Link static libraries of non system libs

--- pyfreezer/freeze.sh
+++ pyfreezer/freeze.sh
@@ -3,11 +3,14 @@
 ./pysrc/python -m pip install -r $(dirname $1)/requirements.txt
 ./pysrc/python ./pysrc/Tools/freeze/freeze.py -p $(realpath pysrc) -e $(realpath ext) -o build "$@"
 cp overrides.c build/
-export LDFLAGS=overrides.c
-cd build && make && cd -
+export LDFLAGS="overrides.c -Wl,-Bstatic"
+cd build && make LIBS="-lcrypt -lutil -Wl,-Bdynamic -lm -lpthread -ldl" && cd -
 cp build/$(basename $1 .py) $(dirname $1)
+
+# Checks
 GLIBC_VER=$(readelf --dyn-syms --wide build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
 echo "Earliest supported glibc: $GLIBC_VER"
 readelf --syms build/$(basename $1 .py) | grep GLIBC_${GLIBC_VER}
+
 echo "ldd:"
 ldd build/$(basename $1 .py)
