c7a15ae7aa8be6e5a7f258614b5b646699e43585
Daniel Shaulov <daniel.shaulov@gmail.com>
2020-01-05 08:23:40 +0200

    ignore weak symbols when checking GLIBC_ compat

--- pyfreezer/freeze.sh
+++ pyfreezer/freeze.sh
@@ -8,7 +8,7 @@ cd build && make LIBS="-Wl,-Bdynamic -lrt -lutil -lpthread -ldl" && cd -
 cp build/$(basename $1 .py) $(dirname $1)
 
 # Checks
-GLIBC_VER=$(readelf --dyn-syms --wide build/$(basename $1 .py) | grep GLIBC_ | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
+GLIBC_VER=$(readelf --dyn-syms --wide build/$(basename $1 .py) | grep GLIBC_ | grep -v WEAK | cut -d@ -f2 | cut -d_ -f2 | cut -d' ' -f1 | sort -V | tail -1)
 echo "Earliest supported glibc: $GLIBC_VER"
 readelf --syms build/$(basename $1 .py) | grep GLIBC_${GLIBC_VER}
 
